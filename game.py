5d2ede9ce52b05d1e7c6a7808acb02f0c54a9274
sp7683 <108552251+sp7683@users.noreply.github.com>
2022-07-17 07:47:40 -0400

    validation complete

--- game.py
+++ game.py
@@ -9,22 +9,26 @@ print("Welcome 'Player One' to Rock, Paper, Scissors, Shoot!")
 # USER INPUTS
 
 user_choice = input("Please select 'Rock', 'Paper', 'Scissors':") 
+user_choice = user_choice.title()
 print(f"You chose: '{user_choice}'")
 
 
 # VALIDATE USER INPUTS
-
-
-
+options = ["Rock", "Paper", "Scissors"]
+if user_choice not in options:
+    print("Invalid Choice. Please play again with a valid choice.")
+    exit()  #quit
 
 # COMPUTER CHOICE
 
-options = ["Rock", "Paper", "Scissors"]
 computer_choice = random.choice(options)
 
 print(f"The computer chose: '{computer_choice}'")
 
 # DETERMINE THE WINNER AND DISPLAY RESULTS
+
+#adapted from code shared in Slack
+#https://nyu-tech-2335.slack.com/archives/C5WPFSB52/p1657672686150239
 
 if user_choice == computer_choice:
     print("You tied!")
